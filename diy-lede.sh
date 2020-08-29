44584a16bd2de4b5595c3c7deefc269a27b8a371
yyetss <757542306@qq.com>
2020-08-29 09:55:20 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -24,7 +24,6 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-git clone https://github.com/xiaorouji/openwrt-package/tree/master/lienol/luci-app-passwall
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge  #主题-edge-动态登陆界面
 #git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
 #git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash  #clash出国软件
